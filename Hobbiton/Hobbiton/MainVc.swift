21f313573567a2551063c5bb1bc4203eb2a46b02
Alberto Hirota <alberto.hirota@gmail.com>
2016-08-06 23:48:49 -0400

    Start Hobbiton Project

---
+++ Hobbiton/Hobbiton/MainVc.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Hobbiton
+//
+//  Created by Alberto Hirota on 8/6/16.
+//  Copyright © 2016 Alberto Hirota. All rights reserved.
+//
+
+import UIKit
+
+class MainVc: UIViewController {
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hobbiton/Hobbiton/aboutHobbitonVc.swift
@@ -0,0 +1,35 @@
+//
+//  aboutHobbitonVc.swift
+//  Hobbiton
+//
+//  Created by Alberto Hirota on 8/6/16.
+//  Copyright © 2016 Alberto Hirota. All rights reserved.
+//
+
+import UIKit
+
+class aboutHobbitonVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
