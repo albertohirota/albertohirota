0f5c93775562b854efc148b07916e4c0be51d523
Alberto Hirota <alberto.hirota@gmail.com>
2016-08-17 14:56:02 -0400

    Starting ShowCase project

---
+++ Showcase/Showcase/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  Showcase
+//
+//  Created by Alberto Hirota on 8/17/16.
+//  Copyright © 2016 Alberto Hirota. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
